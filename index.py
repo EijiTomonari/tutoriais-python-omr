332dfb19da2da670ef74de926ff779bfbcc14315
GabrielTomonariDev <gabriel@tomonari.dev>
2022-03-02 20:50:01 -0300

    Corrige comentários

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-# Inicialza a câmera
-cap = cv.VideoCapture(1) # O número seleciona a câmera
-cap.set(cv.CAP_PROP_FRAME_WIDTH, 1920) # Largura de 1920px
-cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1080) # Altura de 1080px
+# Inicializa a câmera
+cap = cv.VideoCapture(1)  # O número seleciona a câmera
+cap.set(cv.CAP_PROP_FRAME_WIDTH, 1920)  # Largura de 1920px
+cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)  # Altura de 1080px
