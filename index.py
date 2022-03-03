a2b54e381b9d9c62893f9c57e7ab3f1d7adf19cd
GabrielTomonariDev <gabriel@tomonari.dev>
2022-03-02 21:10:39 -0300

    Mostra imagem capturada

--- index.py
+++ index.py
@@ -1,6 +1,17 @@
 import cv2 as cv
 
-# Inicializa a câmera
-cap = cv.VideoCapture(1)  # O número seleciona a câmera
-cap.set(cv.CAP_PROP_FRAME_WIDTH, 1920)  # Largura de 1920px
-cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)  # Altura de 1080px
+
+def capturaImagem():
+    # Inicializa a câmera
+    cap = cv.VideoCapture(1)  # O número seleciona a câmera
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, 1920)  # Largura de 1920px
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)  # Altura de 1080px
+
+    if not cap.isOpened():  # Verifica se a câmera foi inicializada
+        return None
+
+    for i in range(0, 10):  # Lê 10 frames
+        ret, frame = cap.read()
+    cv.imshow("Imagem capturada", frame)  # Mostra a imagem capturada
+    cv.waitKey(0)  # Aguarda infinitamente por uma tecla pressionada
+    return frame
